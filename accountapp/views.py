b61f6ba2827faf425b245424ea86c89075b47d27
Sukhee Kim <kshhyh00@gmail.com>
2023-02-14 02:29:47 +0900

    final new supercreate empty data

--- accountapp/views.py
+++ accountapp/views.py
@@ -8,28 +8,12 @@ from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm, CreateUserForm
-from accountapp.models import HelloWorld
 
 
 has_ownership = [
     login_required, account_ownership_required
 ]
 # Create your views here.
-@login_required
-def hello_world(request):
-        if request.method == 'POST':
-            temp = request.POST.get('hello_world_input')
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hellow_world.html', context={'hello_world_list' : hello_world_list})
-
-
-class UserCreateForm:
-    pass
 
 
 class AccountCreateView(CreateView):
