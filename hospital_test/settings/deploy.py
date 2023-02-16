d2d433badd801545dabdb3bcf96117bd10f92ec0
Sukhee Kim <kshhyh00@gmail.com>
2023-02-16 19:58:57 +0900

    databases settings.py changed

--- hospital_test/settings/deploy.py
+++ hospital_test/settings/deploy.py
@@ -32,24 +32,24 @@ ALLOWED_HOSTS = ["*"]
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'django',
-#         'USER': 'django',
-#         'PASSWORD': 'Iprime103-1403',
-#         'HOST': 'mariadb',
-#         'PORT': '3306'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'prpc8575',
+        'PASSWORD': 'Prpc-8576',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
 
 
 STATIC_ROOT = BASE_DIR / 'static/'
