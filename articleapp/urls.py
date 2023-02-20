c9e82d6db300251c45ed3745af988309f0e12e92
Sukhee Kim <kshhyh00@gmail.com>
2023-02-21 00:57:56 +0900

    imfo added

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('map/', TemplateView.as_view(template_name='articleapp/map.html'), name='map'),
     path('time_op/', TemplateView.as_view(template_name='articleapp/time_op.html'), name='time_op'),
     path('index/', TemplateView.as_view(template_name='articleapp/index.html'), name='index'),
+    path('pm/', TemplateView.as_view(template_name='articleapp/pain_medicine.html'), name='pm'),
+    path('pm_advantage/', TemplateView.as_view(template_name='articleapp/pm_advantage.html'), name='pm_advantage'),
+    path('pm_disease/', TemplateView.as_view(template_name='articleapp/pm_disease.html'), name='pm_disease'),
+    path('privacy_concent/', TemplateView.as_view(template_name='articleapp/privacy_concent.html'), name='privacy_concent'),
 
 ]
