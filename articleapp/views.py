753410975fc4a93ac03651a0878a924ec622fe0c
Sukhee Kim <kshhyh00@gmail.com>
2023-02-23 23:47:34 +0900

    wait_num added bug fix

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,7 +1,3 @@
-from time import strftime
-
-import required as required
-from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
