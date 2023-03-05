5df11b2ffa2ef32397efa8266b60ec174f37e4c4
Sukhee Kim <kshhyh00@gmail.com>
2023-03-05 15:44:57 +0900

    profileupdate page repaired

--- profileapp/forms.py
+++ profileapp/forms.py
@@ -8,10 +8,29 @@ class ProfileCreationForm(ModelForm):
         fields = ['real_name', 'birth_date', 'phone_num']
         labels = {
             'real_name': ('성명'),
-            'birth_date': ('주민번호 앞6자리'),
+            'birth_date': ('주민번호 앞6자리 (생년월일)'),
             'phone_num': ('폰번호')
         }
         help_texts = {
             # 'birth_date': ('주민번호 앞6자리'),
             'phone_num': ('예) 010-1234-5678')
         }
+
+
+class ProfileUpdateForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['real_name', 'birth_date', 'phone_num']
+        labels = {
+            'real_name': ('성명'),
+            'birth_date': ('주민번호 앞6자리 (생년월일)'),
+            'phone_num': ('폰번호')
+        }
+        help_texts = {
+            # 'birth_date': ('주민번호 앞6자리'),
+            'phone_num': ('예) 010-1234-5678')
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['real_name'].disabled = True
--- profileapp/views.py
+++ profileapp/views.py
@@ -6,7 +6,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
 from profileapp.decorators import profile_ownership_required
-from profileapp.forms import ProfileCreationForm
+from profileapp.forms import ProfileCreationForm, ProfileUpdateForm
 from profileapp.models import Profile
 
 
@@ -35,7 +35,7 @@ class ProfileCreateView(CreateView):
 class ProfileUpdateView(UpdateView):
     model = Profile
     # context_object_name = 'target_profile'
-    form_class = ProfileCreationForm
+    form_class = ProfileUpdateForm
     # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
 
