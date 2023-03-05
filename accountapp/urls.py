dc251dea2cf89dd03754a772950dc5a73b762c63
Sukhee Kim <kshhyh00@gmail.com>
2023-03-05 14:54:22 +0900

    privacy policy added

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from django.views.generic import TemplateView
+
 from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
@@ -12,4 +14,6 @@ urlpatterns = [
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
     path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
+    path('agreement/', TemplateView.as_view(template_name='accountapp/agreement.html'), name='agreement'),
+
 ]
