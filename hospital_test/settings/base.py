9cc0dac686f3052d26887486e21b7e01fcd97a12
Sukhee Kim <kshhyh00@gmail.com>
2023-03-09 01:14:23 +0900

    SMTP naver

--- hospital_test/settings/base.py
+++ hospital_test/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 from django.urls import reverse_lazy
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
--- hospital_test/settings/local.py
+++ hospital_test/settings/local.py
@@ -39,15 +39,17 @@ DATABASES = {
 }
 
 #### Email 전송 ####
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # 메일을 호스트하는 서버
-EMAIL_HOST = 'smtp.mail.nate.com'
+EMAIL_HOST = 'smtp.naver.com'
 # gmail과의 통신하는 포트
-EMAIL_PORT = '587'
+EMAIL_PORT = '465'
 # 발신할 이메일
-EMAIL_HOST_USER = 'kshhyh00@nate.com'
+EMAIL_HOST_USER = 'kshhyh00@naver.com'
 # 발신할 메일의 비밀번호
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 # TLS 보안 방법
-EMAIL_USE_TLS = True
+# EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 # 사이트와 관련한 자동응답을 받을 이메일 주소
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
