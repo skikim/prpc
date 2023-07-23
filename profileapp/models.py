d98cde79e1aa317a3277b00fe28ac8ece3cd7341
Sukhee Kim <kshhyh00@gmail.com>
2023-07-23 21:31:32 +0900

    [update] 차트넘버 unique=true 추가

--- profileapp/models.py
+++ profileapp/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     real_name = models.CharField(max_length=16, null=True)
     birth_date = models.CharField(max_length=10, null=True)
     phone_num = models.CharField(max_length=16, null=True)
-    chart_num = models.CharField(max_length=10, null=True, blank=True)
+    chart_num = models.CharField(max_length=10, null=True, unique=True)
 
     def __str__(self):
         return self.real_name
