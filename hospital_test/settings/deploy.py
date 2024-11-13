5e71cff890c833d538f4f4968ed0b7b1e1ad760a
Sukhee Kim <kshhyh00@gmail.com>
2024-11-14 00:59:42 +0900

    cloudfront용 settings.py 수정

--- hospital_test/settings/deploy.py
+++ hospital_test/settings/deploy.py
@@ -27,6 +27,10 @@ SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 ALLOWED_HOSTS = ['prpc.co.kr','www.prpc.co.kr','13.209.205.199','cloudfront.net']
 
 # Database
